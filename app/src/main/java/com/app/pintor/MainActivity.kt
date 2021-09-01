6b57a0c6579a44edec893ff11d65dae81522ebd1
joao.reis <joao.reis@valecard.com.br>
2021-09-01 18:48:49 -0300

    Refactor: refactor struct project

---
+++ app/src/main/java/com/app/pintor/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.app.pintor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.pintor.ui.theme.PintorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PintorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PintorTheme {
+        Greeting("Android")
+    }
+}
