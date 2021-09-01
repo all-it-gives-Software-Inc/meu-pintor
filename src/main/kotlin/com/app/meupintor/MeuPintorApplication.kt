5f969c95ab38902a520b556d0113f2f257c546ba
joao.reis <joao.reis@valecard.com.br>
2021-09-01 14:45:30 -0300

    Configure: Fisrt commit

---
+++ src/main/kotlin/com/app/meupintor/MeuPintorApplication.kt
@@ -0,0 +1,11 @@
+package com.app.meupintor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeuPintorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeuPintorApplication>(*args)
+}
